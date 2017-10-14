d2ce6a48c50b112cd7c87e7c018ea104c1443e09
Marco Serra <marcoserra@pcbuster.it>
2017-10-14 15:04:47 +0200

    Update settings.py

--- c4m/settings.py
+++ c4m/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$32b=!zt69&%l26@m=50r+@by@qhu!f%%^i4-+0#z(n(-sf+z4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.3']
+ALLOWED_HOSTS = []
 
 
 # Application definition
